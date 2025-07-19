8e4f7a3b2e743c50b3e2d704e734b4c9c76f809b
Omkar-pandhekar <141856931+Omkar-pandhekar@users.noreply.github.com>
2025-07-19 22:06:30 +0530

    backend partially completed

--- server/src/index.ts
+++ server/src/index.ts
@@ -7,6 +7,9 @@ import morgan from "morgan";
 import { authMiddleware } from "./middleware/authMiddleware";
 import tenantRoutes from "./routes/tenantRoutes";
 import managerRoutes from "./routes/managerRoutes";
+import propertyRoutes from "./routes/propertyRoutes";
+import leaseRoutes from "./routes/leaseRoutes";
+import applicationRoutes from "./routes/applicationRoutes";
 
 dotenv.config();
 const app = express();
@@ -22,8 +25,11 @@ app.get("/", (req, res) => {
   res.send("This is home route");
 });
 
+app.use("/properties", propertyRoutes);
+app.use("leases", leaseRoutes);
 app.use("/tenants", authMiddleware(["tenant"]), tenantRoutes);
 app.use("/managers", authMiddleware(["manager"]), managerRoutes);
+app.use("/applications", applicationRoutes);
 
 const port = process.env.PORT || 3002;
 app.listen(port, () => {
--- server/src/routes/managerRoutes.ts
+++ server/src/routes/managerRoutes.ts
@@ -1,9 +1,16 @@
 import express from "express";
-import { getManager, createManager } from "../controllers/managerControllers";
+import {
+  getManager,
+  createManager,
+  updateManager,
+  getManagerProperties,
+} from "../controllers/managerControllers";
 
 const router = express.Router();
 
 router.get("/:cognitoId", getManager);
 router.post("/", createManager);
+router.put("/:cognitoId", updateManager);
+router.get("/:cognitoId/properties", getManagerProperties);
 
 export default router;
