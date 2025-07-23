d3a6e999da191d7e884a0b3fed1f60e50ed68d34
Omkar-pandhekar <141856931+Omkar-pandhekar@users.noreply.github.com>
2025-07-23 10:07:08 +0530

    added tenants and managers pages

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,3 +1,13 @@
+import Navbar from "@/components/layouts/Navbar";
+import Landing from "./(nondashboard)/landing/page";
+
 export default function Home() {
-  return <div className="bg-black">Home</div>;
+  return (
+    <div className="h-full w-full">
+      <Navbar />
+      <main className={`h-full flex w-full flex-col`}>
+        <Landing />
+      </main>
+    </div>
+  );
 }
